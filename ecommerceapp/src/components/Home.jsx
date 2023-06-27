d0c89e554c3e33670dd8286fc1d26de3b6edab5e
João Ferreira <joaomgsferreira@outlook.com>
2023-06-27 17:32:24 +0100

    Ecommerce Startup

---
+++ ecommerceapp/src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Carousel  from './Subcomponents/Carousel/Carousel'
+import classes from './Home.module.css'
+
+function Home() {
+  return (
+    <div class={classes.wrapper}>
+      <main class={classes.main}>
+        <h1>Grab Up To 50% Off On The Selected SunGlasses</h1>
+        <
+      </main>
+    </div>
+  )
+}
+
+export default Home
