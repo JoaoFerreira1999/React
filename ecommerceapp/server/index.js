0a1055e2f6a7d1899dff923aa5b24d053a45ae20
João Ferreira <joaomgsferreira@outlook.com>
2023-08-26 03:42:42 +0100

    JWT implementation

--- ecommerceapp/server/index.js
+++ ecommerceapp/server/index.js
@@ -5,14 +5,22 @@ const axios = require('axios');
 const mongoose = require('mongoose');
 const connectDB = require('./dbOperations/dbConn')
 const glassesSchema = require('./Schemas/glassesSchema')
+const userModel = require('./Schemas/dbUserSchema')
+const bcrypt = require('bcrypt');
 require('dotenv').config();
+
+const jwt  = require('jsonwebtoken');
 
 const app = express();
 
 //connect to MongoDB
 connectDB();
 
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST'],
+    credentials: true,
+}));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
@@ -33,9 +41,75 @@ app.get('/shop', async (req, res) =>{
     res.send(hotDeals);
 });
 
-app.post('/register', (req, res) => {
+const saltRounds = 10;
+
+app.post('/register', async (req, res) => {
     console.log(req.body);
-})
+    
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+        bcrypt.hash(password, salt, async function(err, hash) {
+            console.log(salt);
+            const user = new userModel({
+                name: name,
+                email: email,
+                password: hash
+            });
+
+            const userExists = await userModel.exists({email: email});
+
+            if(userExists) {
+                res.json({
+                    auth: false,
+                    message: "Email already in use!"
+                })
+            } else {
+                user.save();
+                console.log("User Registered Successfully!");
+
+                const registeredDetails = [name, email];
+
+                const accessToken = jwt.sign({email}, process.env.JWT_SECRET, {
+                    expiresIn: 300,
+                });
+
+                res.json({
+                    auth: true,
+                    token: token,
+                    details: registeredDetails,
+                });
+            }
+        });
+    })
+});
+
+
+const verifyJWT = (req, res, next) => {
+    const token = req.headers["authorization"];
+
+    if(!token) {
+        res.send("Token Required!")
+    } else {
+        jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+            if(err){
+                res.json({
+                    auth: false,
+                    message: "Failed to Authenticate!"
+                })
+            } else {
+                req.email = user;
+                next();
+            }
+        })
+    }
+}
+
+app.get('/isUserAuth', verifyJWT , (req, res) => {
+    res.send("Successfully Authenticated!");
+});
 
 
 mongoose.connection.once('open', () => {
